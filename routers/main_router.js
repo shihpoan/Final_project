24c86b98ab2b9aede0b6957547db16424c74cafc
shihpoan <randypunk123@gmail.com>
2021-06-27 19:37:14 +0800

    first commit

---
+++ routers/main_router.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+let Shihpoan = require('./Shipoan.js');
+let Naruto = require('./Naruto.js');
+let Sasuke = require('./Sasuke.js');
+router.use(express.static('./image')); 
+router.get('/', (req, res) => {
+    let options = {
+        root: __dirname,
+        dotfiles: 'deny'
+    }
+    res.sendFile("index.html", options)
+})
+router.use('/Shihpoan', Shihpoan);
+router.use('/Naruto', Naruto);
+router.use('/Sasuke', Sasuke);
+
+module.exports = router;
